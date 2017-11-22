443129faa8126bec1545c3dd38b622f86c4ce726
Anil Chaudhari <imanilchaudhari@gmail.com>
2017-11-22 22:49:00 +0545

    Updated README.md

--- Cache/Adapter/AbstractAdapter.php
+++ Cache/Adapter/AbstractAdapter.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace imanilchaudhari\CurrencyConverter\Cache\Adapter;
 
 use DateInterval;
--- Exception/ExceptionInterface.php
+++ Exception/ExceptionInterface.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace imanilchaudhari\CurrencyConverter\Exception;
 
 interface ExceptionInterface
